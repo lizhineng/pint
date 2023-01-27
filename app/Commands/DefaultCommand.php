9c624b4504e6ff594e037d0f6922fc5936c19b72
Li Zhineng <im@zhineng.li>
2023-01-27 15:26:21 +0800

    provides `--changed` option

--- app/Commands/DefaultCommand.php
+++ app/Commands/DefaultCommand.php
@@ -39,6 +39,7 @@ class DefaultCommand extends Command
                     new InputOption('preset', '', InputOption::VALUE_REQUIRED, 'The preset that should be used'),
                     new InputOption('test', '', InputOption::VALUE_NONE, 'Test for code style errors without fixing them'),
                     new InputOption('dirty', '', InputOption::VALUE_NONE, 'Only fix files that have uncommitted changes'),
+                    new InputOption('changed', '', InputOption::VALUE_NONE, 'Only fix files that have been changed since the origin branch'),
                     new InputOption('format', '', InputOption::VALUE_REQUIRED, 'The output format that should be used'),
                 ]
             );
